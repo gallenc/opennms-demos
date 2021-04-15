578a36f8213cc6679799134582aad8ef82288a6c
gallenc <craig.gallen@entimoss.com>
2021-04-15 18:05:58 +0100

    added docker compose for opennms-har

---
+++ opennms-har-demo/simple-horizon-image-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# set -x 
+echo create docker image from opennms tarball. Run this script in diectory above opennms git repository
+
+branch=$(git -C ./opennms branch --show-current)
+name="${branch//'/'/"_"}"
+echo creating docker image  $branch  in $name.oci
+
+cp -v opennms/target/*SNAPSHOT.tar.gz  opennms/opennms-container/horizon/tarball/
+
+echo docker build --tag="opennms/horizon:$name" opennms/opennms-container/horizon/
+docker build --tag="opennms/horizon:$name" opennms/opennms-container/horizon/
+
+echo docker image save "opennms/horizon:$name" -o opennms/opennms-container/horizon/images/horizon_$name.oci
+docker image save "opennms/horizon:$name" -o opennms/opennms-container/horizon/images/horizon_$name.oci
+
